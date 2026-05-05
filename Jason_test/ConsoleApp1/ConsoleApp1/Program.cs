d7acc52840af45110e6659644e87b225a751a082
JackyKCFan <jackykcfan@fih-foxconn.com>
2026-05-05 10:23:22 +0800

    MODIFY
    PRACTICE

---
+++ Jason_test/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using ConsoleApp1;
+using System.Text.Json;
+
+var client =new HttpClient();
+
+var responce = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos/19");
+var todo = JsonSerializer.Deserialize<Todo>(responce);
+
+var jso=new JsonSerializerOptions { WriteIndented = true };
+var jsonstr= JsonSerializer.Serialize(todo, jso);
+
+var ListResponce = await client.GetStringAsync("https://jsonplaceholder.typicode.com/todos");
+var Todos = JsonSerializer.Deserialize<List<Todo>>(ListResponce,
+    new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+    );
+return;
+
---
+++ Jason_test/ConsoleApp1/ConsoleApp1/todo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   
+    public class Todo
+    {
+
+        //Jsonignore,jsonconverter
+        [JsonPropertyName("userId")]
+        public int UserId { get; set; }
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+        [JsonPropertyName("completed")]
+        public bool Completed { get; set; }
+    }
+
+}
